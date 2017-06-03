e0bcba120d703fd34585ae218e19cf6279180f34
Karsten Röthig <karsten@Karstens-MacBook-Pro.local>
2017-06-03 10:32:23 +0200

    relocated config classes

--- src/main/java/karstenroethig/laeufe/config/WebMvcConfig.java
+++ src/main/java/karstenroethig/laeufe/config/WebMvcConfig.java
@@ -1,18 +1,16 @@
-package karstenroethig.laeufe;
-
-import karstenroethig.laeufe.controller.formatter.CountryFormatter;
-import karstenroethig.laeufe.controller.formatter.OrganizerFormatter;
+package karstenroethig.laeufe.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
 import org.springframework.format.FormatterRegistry;
-
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+import karstenroethig.laeufe.controller.formatter.CountryFormatter;
+import karstenroethig.laeufe.controller.formatter.OrganizerFormatter;
 
 
 @Configuration
