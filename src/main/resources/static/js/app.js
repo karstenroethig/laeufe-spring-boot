f0b477afd4a8cd7372cc12e98fd32ea84d92f726
Karsten Röthig <karstenroethig@users.noreply.github.com>
2018-03-20 08:52:07 +0100

    updated dependencies
    spring boot 1.5.10.RELEASE
    jquery 3.3.1
    bootstrap 3.3.7
    patternfly 3.40.0

---
+++ src/main/resources/static/js/app.js
@@ -0,0 +1,39 @@
+
+$( document ).ready( function() {
+
+	$( function () {
+		
+		// initialize bootstrap tooltips
+		$('[data-toggle="tooltip"]').tooltip();
+		
+		// initialize bootstrap switch
+		$('.bootstrap-switch').bootstrapSwitch();
+		
+		// initialize bootstrap datepicker
+		$( '.input-group.date' ).datepicker({
+			format: "dd.mm.yyyy",
+			weekStart: 1,
+			clearBtn: true,
+			todayBtn: "linked",
+			todayHighlight: true,
+			orientation: "bottom left",
+			language: "de",
+			daysOfWeekHighlighted: "0,6",
+			calendarWeeks: true,
+			autoclose: true
+		});
+	});
+	
+	// delete modals: transfer the id to the modal form
+	$( '#deleteModal' ).on( 'show.bs.modal', function( event ) {
+		var button = $( event.relatedTarget ); // Button that triggered the modal
+		var id = button.data( 'id' ); // Extract info from data-* attributes
+
+		// Update the modal's content.
+		var modal = $( this );
+		var link = modal.find( '.btn-danger' );
+		var template = link.data( 'href-template' );
+		link.attr( 'href', template.replace( '{id}', id ) );
+	});
+
+});
