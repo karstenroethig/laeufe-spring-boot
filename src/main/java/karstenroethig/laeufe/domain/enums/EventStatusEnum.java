4a8d1c167f3a384145985a7acd3d4eae6812b1c1
Karsten Röthig <karstenroethig@users.noreply.github.com>
2018-07-26 11:03:13 +0200

    added status CANCELED

--- src/main/java/karstenroethig/laeufe/domain/enums/EventStatusEnum.java
+++ src/main/java/karstenroethig/laeufe/domain/enums/EventStatusEnum.java
@@ -12,13 +12,15 @@ public enum EventStatusEnum
 	 * - completed: if there is at least one race with status completed
 	 * - failed: if races have only status failed or status dnp
 	 * - dnp: only if all races have status dnp
+	 * - canceled: only if all races have status canceled
 	 */
 
 	PLANED( 0, "fa fa-calendar", "label label-default" ),
 	REGISTERED( 1, "fa fa-calendar-check-o", "label label-success" ),
 	COMPLETED( 2, "pficon pficon-ok", "label label-success" ),
 	FAILED( 3, "pficon pficon-error-circle-o", "label label-danger" ),
-	DNP( 4, "pficon pficon-delete", "label label-danger" );
+	DNP( 4, "pficon pficon-delete", "label label-danger" ),
+	CANCELED( 5, "fa fa-ban", "label label-danger" );
 
 	private int key;
 	private String icon;
